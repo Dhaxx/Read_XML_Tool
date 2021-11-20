393dd9e8ac6b5cf33dfc32af642ee83a6f344703
unknown <Kaiopablo44@gmail.com>
2021-11-20 01:12:00 -0300

    Acessando subTags e manipulando a saída para melhor visualização

--- work.py
+++ work.py
@@ -1,9 +1,7 @@
 import xml.etree.ElementTree as ET
 
-tree = ET.parse('File.XML') # Here go your XML file name
+tree = ET.parse('work.xml')
 root = tree.getroot()
-
-filtro = '*'
-
-for row in root.iter(filtro): 
-    print(row.tag, row.attrib)
+for row in root.iter('ROW'): # <--- vai procurar somente por ROW
+    print("CODIGO_BAI: " + row.attrib['Column2'] + " | " + "EMPRESA_BAI" + row.attrib['Column3'] + " | " + "DESCRICAO_BAI: " + row.attrib['Column4'] + " | " + "CODIGO_TCE_BAI: " + row.attrib['Column5'] + " | " + "CHK_DESM_BAI: " + row.attrib['Column6']) 
+    print("\n")
